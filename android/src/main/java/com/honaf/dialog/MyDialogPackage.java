b9adf033b51ae893a477419bda2f36e7db52e835
a.voityuk <a.voityuk@gmail.com>
2018-09-21 16:37:54 +0300

    remove @Override for createJSModules

--- android/src/main/java/com/honaf/dialog/MyDialogPackage.java
+++ android/src/main/java/com/honaf/dialog/MyDialogPackage.java
@@ -22,7 +22,7 @@ public class MyDialogPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+    // Deprecated in RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
